71129302657350ff57025efca3baa45d0452383c
Racem Benrhayem <racembenrhayem@insat.u-cartage.tn>
2022-03-31 10:02:21 +0100

    fixing user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -12,11 +12,14 @@ User.init({
         primaryKey : true ,
         autoIncrement : true ,
     },
-    username: {
+    firstName: {
         type: DataTypes.STRING,
         allowNull : false
     },
-  
+    lastName : {
+        type: DataTypes.STRING,
+        allowNull : false
+    } ,
     email : {
         type :DataTypes.STRING,
         allowNull : false ,
